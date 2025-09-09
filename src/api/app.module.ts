09c3c79252e84cc58c9747e5630b286c494d72ce
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-09-09 22:18:18 +0500

    up

--- src/api/app.module.ts
+++ src/api/app.module.ts
@@ -14,6 +14,7 @@ import { TicketModule } from './post/ticket/ticket.module';
 import { OrderModule } from './post/order/order.module';
 import { PaymentModule } from './post/payment/payment.module';
 import { WalletModule } from './post/wallet/wallet.module';
+import { AuthModule } from './user/auth/auth.module';
 
 @Module({
   imports: [
@@ -40,6 +41,7 @@ import { WalletModule } from './post/wallet/wallet.module';
     OrderModule,
     PaymentModule,
     WalletModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
