ec1c319ea4f270d0da811ff22de37903aeec4be3
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-09-09 15:12:33 +0500

    up

--- src/api/app.module.ts
+++ src/api/app.module.ts
@@ -2,7 +2,18 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { config } from 'src/config/env.config';
-import { AdminModule } from './admin/admin.module';
+import { AdminModule } from './user/admin/admin.module';
+import { CustomerModule } from './user/customer/customer.module';
+import { GenreModule } from './post/genre/genre.module';
+import { ReviewModule } from './post/review/review.module';
+import { MovieModule } from './post/movie/movie.module';
+import { CountryModule } from './post/country/country.module';
+import { RoomModule } from './post/room/room.module';
+import { ShowtimeModule } from './post/showtime/showtime.module';
+import { TicketModule } from './post/ticket/ticket.module';
+import { OrderModule } from './post/order/order.module';
+import { PaymentModule } from './post/payment/payment.module';
+import { WalletModule } from './post/wallet/wallet.module';
 
 @Module({
   imports: [
@@ -17,6 +28,17 @@ import { AdminModule } from './admin/admin.module';
       global: true,
     }),
     AdminModule,
+    CustomerModule,
+    GenreModule,
+    ReviewModule,
+    MovieModule,
+    CountryModule,
+    RoomModule,
+    ShowtimeModule,
+    TicketModule,
+    OrderModule,
+    PaymentModule,
+    WalletModule,
   ],
 })
 export class AppModule {}
--- src/api/user/admin/admin.module.ts
+++ src/api/user/admin/admin.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { AdminController } from './admin.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  // imports:[TypeOrmModule.forFeature([AdminE])],
   controllers: [AdminController],
   providers: [AdminService],
 })
