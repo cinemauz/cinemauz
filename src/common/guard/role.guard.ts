f96e21ef2ebf1b48da98b49c25d172e3ca7c6259
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-09-11 00:19:00 +0500

    up

--- src/common/guard/role.guard.ts
+++ src/common/guard/role.guard.ts
@@ -1,21 +1,30 @@
-import { CanActivate, ExecutionContext, ForbiddenException, Injectable } from "@nestjs/common";
-import { Reflector } from "@nestjs/core";
-import { Observable } from "rxjs";
-import { ROLES_KEY } from "../decorator/roles.decorator";
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+import { ROLES_KEY } from '../decorator/roles.decorator';
 
 @Injectable()
-export class RolesGuard implements CanActivate{
-    constructor(private readonly reflector:Reflector){}
+export class RolesGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
 
-    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-        const requiredRole=this.reflector.getAllAndOverride<string[]>(
-            ROLES_KEY,
-            [context.getHandler(),context.getClass()]
-        )
-        const {user}=context.switchToHttp().getRequest()
-        if(!requiredRole.includes(user?.roles)){
-            throw new ForbiddenException('Forbiden user')
-        }
-        return true
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const requiredRole = this.reflector.getAllAndOverride<string[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    const { user } = context.switchToHttp().getRequest();
+
+    if (!requiredRole.includes(user?.role)) {
+      throw new ForbiddenException('Forbiden user');
     }
+    return true;
+  }
 }
