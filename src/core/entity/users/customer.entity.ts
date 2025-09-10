38947661583b27bccdaed2b3bd7005178f69ba69
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-09-10 09:03:01 +0500

    entity add

---
+++ src/core/entity/users/customer.entity.ts
@@ -0,0 +1,24 @@
+import { BaseEntity } from "src/common/database/base.entity";
+import { Roles } from "src/common/enum/Roles";
+import { Column, Entity } from "typeorm";
+
+@Entity('customer')
+export class CustomerEntity extends BaseEntity{
+      @Column({ type: 'varchar' })
+      name: string;
+    
+      @Column({ type: 'varchar', unique: true })
+      email: string;
+    
+      @Column({ type: 'varchar' })
+      hashed_password: string;
+    
+      @Column({ type: 'enum', enum: Roles, default: Roles.CUSTOMER })
+      role: string;
+    
+      @Column({ type: 'boolean', default: true })
+      is_active: boolean;
+
+      @Column({ type: 'int', default:0})
+      balance: boolean;
+}
