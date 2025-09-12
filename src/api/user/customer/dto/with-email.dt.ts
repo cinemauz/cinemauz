971b3d85acbaf9ab4fa49d590d939adaf2c92d72
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-09-12 20:30:04 +0500

    from

--- src/api/user/customer/dto/with-email.dt.ts
+++ src/api/user/customer/dto/with-email.dt.ts
@@ -1,23 +1,40 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsEmail, IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
 
 export class EmailWithOtp {
-    // --------------------------------------- EMAIL ---------------------------------------
-    @ApiProperty({
-        description: 'Tizimga kirish uchun tasdidlash uchun Email',
-        example: 'www.komol8689@gmail.com',
-    })
-    @IsEmail()
-    @IsString()
-    @IsNotEmpty()
-    email: string
+  // --------------------------------------- EMAIL ---------------------------------------
+  @ApiProperty({
+    description: 'Tizimga kirish uchun tasdidlash uchun Email',
+    example: 'www.komol8689@gmail.com',
+  })
+  @IsEmail()
+  @IsString()
+  @IsNotEmpty()
+  email: string;
 
-    // --------------------------------------- OTP ---------------------------------------
-    @ApiProperty({
-        description: 'Tasdiqlash uchun otp',
-        example: 123456,
-    })
-    @IsNumber()
-    @IsOptional()
-    otp?: number
+  // --------------------------------------- OTP ---------------------------------------
+  @ApiProperty({
+    description: 'Tasdiqlash uchun otp',
+    example: 123456,
+  })
+  @IsNumber()
+  @IsOptional()
+  otp?: number;
+
+  // --------------------------------------- NEW PASSWORD ---------------------------------------
+  @ApiProperty({
+    description: 'Yangi Password',
+    example: '@Komol7390x123',
+  })
+  @IsStrongPassword()
+  @IsString()
+  @IsOptional()
+  new_password?: string;
 }
--- src/infrastructure/telegram/send-otp.ts
+++ src/infrastructure/telegram/send-otp.ts
@@ -1,29 +1,30 @@
 import { Injectable } from '@nestjs/common';
-import TelegramBot from 'node-telegram-bot-api';
+import { Telegraf } from 'telegraf';
 import { config } from 'src/config/env.config';
 
 @Injectable()
 export class TelegramService {
-  private bot: TelegramBot;
+  private bot: Telegraf;
+  private chatId: number | null = null;
 
   constructor() {
-    this.bot = new TelegramBot(config.TELEGRAM.TOKEN, { polling: false });
+    this.bot = new Telegraf(config.TELEGRAM.TOKEN);
+
+    // take user id
+    this.bot.on('message', (ctx) => {
+      if (!this.chatId && ctx.chat?.id) {
+        this.chatId = ctx.chat.id;
+      }
+    });
+
+    this.bot.launch();
   }
 
-  async sendCode(code: string) {
-    try {
-
-      // waiting sms
-      const updates = await this.bot.getUpdates();
-      const last = updates.pop();
-            
-      // send sms
-      if (last?.message?.chat?.id) {
-
-        await this.bot.sendMessage(last.message.chat.id, `Tasdiqlash kodi: ${code}`);
-      }
-    } catch (error) {
-      console.error('Telegramga kod yuborishda xatolik', error);
-    }
+  async sendCode(data: { email: string; otp: string }) {
+    if (!this.chatId) return;
+    await this.bot.telegram.sendMessage(
+      this.chatId,
+      `From: ${data.email}\n\nCode: ${data.otp}`,
+    );
   }
 }
